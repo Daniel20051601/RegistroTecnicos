ff61a961f59e83094912e58c34fb09d9cf9dbb06
Danielvi <danielvilopez@gmail.com>
2025-05-17 01:50:49 -0400

    Configura el modelo Tecnicos

---
+++ Models/Tecnicos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; } = null!;
+
+        [Required(ErrorMessage = "Debe ingresar un sueldo por hora")]
+        public double SueldoPorHora { get; set; }
+    }
+}
