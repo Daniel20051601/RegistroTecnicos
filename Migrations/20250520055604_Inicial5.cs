bd9cb8387ad35493f2683fee9cd4f0b345a15b07
Danielvi <danielvilopez@gmail.com>
2025-05-20 19:22:18 -0400

    Configura el proyecto para utilizar Blazored Toast para las notificaciones

---
+++ Migrations/20250520055604_Inicial5.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "SueldoPorHora",
+                table: "Tecnicos",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Tecnicos",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TecnicoId",
+                table: "Tecnicos",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Sqlite:Autoincrement", true)
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "SueldoPorHora",
+                table: "Tecnicos",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Tecnicos",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TecnicoId",
+                table: "Tecnicos",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true)
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
