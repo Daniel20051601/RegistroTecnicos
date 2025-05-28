00d6104a78e71de27e39cafa2a339f87c3f5fa50
Danielvi <danielvilopez@gmail.com>
2025-05-28 14:39:52 -0400

    Crea y condifgura la clase Tickets

---
+++ Models/Tickets.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Tickets
+{
+    [Key]
+    public int TicketId { get; set; }
+    
+    public DateTime Fecha { get; set; }
+
+    [Required(ErrorMessage = "Seleccione una Prioridad para el Ticket")]
+    public string Prioridad { get; set; } = null!;
+
+    [Required(ErrorMessage = "Debe seleccionar un Cliente")]
+    public int ClienteId { get; set; }
+
+    [Required(ErrorMessage = "Debe ingresar el asunto del Ticket")]
+    public string Asunto { get; set; } = null!;
+
+    [Required(ErrorMessage = "Ingrese una descripción para el Ticket")]
+    public string Descripcion { get; set; } = null!;
+
+    [Required(ErrorMessage ="Ingrese el tiempo invertido")]
+    public double TiempoInvertido { get; set; }
+
+    [Required(ErrorMessage = "Selecione un Tecnico Encargado")]
+    public int TecnicoId { get; set; }
+
+    public Tecnicos Tecnico { get; set; } = null!;
+    public Clientes Cliente { get; set; } = null!;
+}
