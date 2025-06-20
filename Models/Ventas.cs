6cb735383444f680292633a8151f6d3312a2ce50
Danielvi <danielvilopez@gmail.com>
2025-06-19 23:47:02 -0400

    Crea y configura el Modelo Ventas

---
+++ Models/Ventas.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class Ventas
+{
+    [Key]
+    public int VentaId { get; set; }
+
+    [Required]
+    public DateTime Fecha { get; set; } = DateTime.UtcNow;
+
+    public int ClienteId { get; set; }
+
+    [InverseProperty("Venta")]
+    public virtual ICollection<VentasDetalle> VentasDetalles { get; set; } 
+
+    [ForeignKey("ClienteId")]
+    [InverseProperty("Ventas")]
+    public virtual Clientes Cliente { get; set; } = null!;
+
+}
