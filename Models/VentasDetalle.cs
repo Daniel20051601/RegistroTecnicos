f6f090ca3dbceaa22b1cedc3d0971fdfed782051
Danielvi <danielvilopez@gmail.com>
2025-06-19 23:47:27 -0400

    Crea y configura el modelo VentasDetalle

---
+++ Models/VentasDetalle.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class VentasDetalle
+{
+    [Key]
+    public int VentasDetalleId { get; set; }
+
+    [Required]
+    public int Cantidad { get; set; }
+
+    [Required]
+    public decimal Monto { get; set; }
+
+    public int SistemaId { get; set; }
+
+    public int VentaId { get; set; }
+
+    [ForeignKey("VentaId")]
+    [InverseProperty("VentasDetalles")]
+    public virtual Ventas Venta { get; set; } = null!;
+
+    [ForeignKey("SistemaId")]
+    public Sistemas Sistema { get; set; }
+
+}
