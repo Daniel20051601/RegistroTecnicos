1ce1c581d09f395691ad1297c489d2b7f78a4c89
Danielvi <danielvilopez@gmail.com>
2025-06-19 23:46:35 -0400

    Actualiza el Modelo de Sistemas para agregar nuevos campos

--- Models/Sistemas.cs
+++ Models/Sistemas.cs
@@ -9,7 +9,7 @@ public class Sistemas
 
     [Required(ErrorMessage = "Debe agregar la descripción de este sistema")]
     [StringLength(300, ErrorMessage = "La descripción no puede tener mas de 300 caracteres")]
-    [RegularExpression(@"^(?=.*[A-Za-z])[^\d]*\S[^\d]*$", 
+    [RegularExpression(@"^(?=.*[A-Za-z]).*\S.*$", 
         ErrorMessage = "La descripción debe contener al menos una letra, no puede ser solo números o espacios.")]
     public string DescripcionSistema { get; set; } = null!;
 
@@ -18,5 +18,11 @@ public class Sistemas
 
     public DateTime Fecha { get; set; } = DateTime.Now;
 
+    [Required(ErrorMessage = "Debe agregar la cantidad en Existencia de este Sistema")]
+    public int Existencia { get; set; }
 
+    [Required(ErrorMessage = "Debe agregar el precio del Sistema")]
+    public decimal precio { get; set; }
+
+    public ICollection<VentasDetalle> VentasDetalle { get; set; } = new List<VentasDetalle>();
 }
